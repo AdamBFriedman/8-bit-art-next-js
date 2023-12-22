9f20b7ab54b4e95329e536acabfa3c8d9f3c1aea
adambfriedman <adam.friedman5@gmail.com>
2023-12-22 11:49:16 -0600

    Add Scrooge McDuck.

---
+++ src/Components/Disney/Scrooge.tsx
@@ -0,0 +1,212 @@
+import React from 'react';
+
+export default function Scrooge() {
+  return (
+    <div className="mb">
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="scroogeBlue" />
+        <div className="scroogeBlue" />
+        <div className="scroogeBlue" />
+        <div className="scroogeBlue" />
+        <div className="scroogeBlue" />
+        <div className="scroogeBlue" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="white" />
+        <div className="scroogeTeal" />
+        <div className="scroogeTeal" />
+        <div className="white" />
+        <div className="white" />
+        <div className="scroogeTeal" />
+        <div className="scroogeTeal" />
+        <div className="white" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="white" />
+        <div className="scroogeTeal" />
+        <div className="black" />
+        <div className="white" />
+        <div className="white" />
+        <div className="black" />
+        <div className="scroogeTeal" />
+        <div className="white" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="white" />
+        <div className="scroogeOrange" />
+        <div className="scroogeTeal" />
+        <div className="black" />
+        <div className="white" />
+        <div className="white" />
+        <div className="black" />
+        <div className="scroogeTeal" />
+        <div className="scroogeOrange" />
+        <div className="white" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="white" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="white" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="white" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="white" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="scroogeOrange" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeYellow" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="scroogeRed" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="black" />
+        <div className="scroogeRed" />
+        <div className="greyBackground" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="white" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeRed" />
+        <div className="scroogeBrown" />
+        <div className="scroogeBrown" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="white" />
+        <div className="white" />
+        <div className="white" />
+        <div className="white" />
+        <div className="white" />
+        <div className="white" />
+        <div className="greyBackground" />
+        <div className="scroogeBrown" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="white" />
+        <div className="white" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="white" />
+        <div className="white" />
+        <div className="greyBackground" />
+        <div className="scroogeBrown" />
+      </div>
+      <div className="scrooge wrapper">
+        <div className="greyBackground" />
+        <div className="scroogeOrange" />
+        <div className="scroogeBlue" />
+        <div className="scroogeBlue" />
+        <div className="greyBackground" />
+        <div className="greyBackground" />
+        <div className="scroogeBlue" />
+        <div className="white" />
+        <div className="scroogeOrange" />
+        <div className="scroogeBrown" />
+      </div>
+    </div>
+  );
+}
